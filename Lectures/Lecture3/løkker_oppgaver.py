65906b39c92080a05d10e78c7d4acf3c6f8e06cf
aa1202 <andreas.amundsen123@gmail.com>
2019-02-26 11:18:35 +0100

    Legg til løkker_oppgaver

---
+++ Lectures/Lecture3/løkker_oppgaver.py
@@ -0,0 +1,16 @@
+# Oppgave 1 - enkel
+#
+# Lag en løkke som skriver ut 'Hei Verden!' tre ganger
+#
+
+# Oppgave 2 - middels
+#
+# Lag et program bestående av to løkker som først skriver ut 'Kaffe' tre ganger
+# og 'Te' 4 ganger
+#
+
+
+# Oppgave 3 - vanskelig
+#
+# Lag en løkke som skriver ut tallene fra 0-9
+#
