9011496bc3e60779d9d81c0e2defc61732d77670
Lekesoldat <magnulho@gmail.com>
2019-02-26 15:33:55 +0100

    Linting.

--- Lectures/Lecture3/fasit.py
+++ Lectures/Lecture3/fasit.py
@@ -101,5 +101,3 @@ for i in range(3):
 #
 for i in range(10):
     print(i)
-
-
--- Lectures/Lecture3/funksjoner.py
+++ Lectures/Lecture3/funksjoner.py
@@ -5,9 +5,11 @@ Tema: Funksjoner
 #
 # Lag en funksjon som heter "siNoe" som printer teksten "Hei, verden!"
 
+
 # Oppgave 2 - Enkel
 #
 # Lag en funksjon som heter "visTegn" som printer ut et valgritt tegn, - for eksempel: *, +, -, !, ., 
+
 
 # Oppgave 3 - Middels
 #
@@ -15,16 +17,19 @@ Tema: Funksjoner
 # 1. Lagrer navnet ditt i en variabel som heter "navn"
 # 2. Printer ut "Hei, <navnet ditt>"
 
+
 # Oppgave 4 - Middels
 #
 # Lag en funksjon som heter "regnUt" som gjør følgende:
 # 1. Lagrer to tall i variabler. Kall de to variablene "tall1" og "tall1". Du bestemmer selv hvilke tall det skal være.
 # 2. Print ut tall1 og tall2 plusset sammen.
 
+
 # Oppgave 5 - vanskelig
 #
 # Lag først en funksjon som printer ut teksten "Hei, verden!". Lag deretter en løkke som går fra 0 til 10.
 # Kall funksjonen i løkken slik at "Hei, verden!" printes 10 ganger.
+
 
 # Oppgave 6 - vanskelig
 #
